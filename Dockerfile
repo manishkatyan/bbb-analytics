467af0822a6a2540e8ecf42eaa63beebefa0a4f1
arunkumar141997 <arun@higheredlab.com>
2021-10-28 15:33:04 +0000

    initial Commit

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM node:14
+
+# Create app directory
+WORKDIR /usr/src/app
+
+# Install app dependencies
+
+COPY package*.json ./
+RUN mkdir -p  /usr/src/app/learning-dashboard
+RUN npm install
+# If you are building your code for production
+RUN npm ci --only=production
+
+# Bundle app source
+COPY . .
+
+EXPOSE 8080
+CMD [ "node", "server.js" ]
---
+++ server.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+const express = require('express');
+require('dotenv').config()
+const PORT = 3000;
+const BASE_URL = process.env.BASE_URL
+
+const app = express();
+app.use(express.json({limit: '1mb'}));
+app.use(express.urlencoded({extended: true,limit: '1mb'}));
+app.use(express.static('public'));
+
+app.post('/api/v1/analytics/', async (req, res) => {
+  let data = {}
+  let meetingId, language, token
+  meetingId = req.body.meeting_id
+  language = req.body.language
+  try {
+     token = await fs.readdirSync(`/usr/src/app/learning-dashboard/s${meetingId}`)
+     data["analytics_url"] = `${BASE_URL}/learning-dashboard/?meeting=${meetingId}&report=${token}&lang=${language}`
+     await  res.json(data);
+    } catch (e) {
+      res.sendStatus(404);
+    }
+  });
+
+app.get('/api/v1/check',  (req, res) => {
+   res.json({'status': 'ok'})
+})
+
+app.listen(PORT, () => {
+    console.log("Server running on port 3000");
+   });
