fffda883aa721f6c996e9de475b443da261df6a6
arunkumar141997 <arun@higheredlab.com>
2022-03-12 10:24:16 +0530

    Intial commit

---
+++ exportData.js
@@ -0,0 +1,18 @@
+const fs = require('fs-extra')
+const ANALYTICS_FOLDER_PATH = '/var/bigbluebutton/learning-dashboard'
+const data = {
+    analytics: []
+}
+
+fs.readdirSync(ANALYTICS_FOLDER_PATH).filter(function (meetingId) {
+    let token
+    if (meetingId.length > 40) {
+        token = fs.readdirSync(`${ANALYTICS_FOLDER_PATH}/${meetingId}`)
+        if (token) {
+            const name = fs.readJsonSync(`${ANALYTICS_FOLDER_PATH}/${meetingId}/${token[0]}/learning_dashboard_data.json`).name
+            data.analytics.push({ name, meetingId, token: token[0] })
+        }
+    }
+})
+fs.writeJsonSync('./data.json', data)
+
