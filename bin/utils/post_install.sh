49cbde7815bca51b9841d7549600f2da803a8e3b
arunkumar141997 <arun@higheredlab.com>
2022-03-14 22:11:35 +0530

    fix global install path

--- bin/utils/post_install.sh
+++ bin/utils/post_install.sh
@@ -3,18 +3,19 @@ BIGBLUEBUTTON="/etc/bigbluebutton/bigbluebutton-release"
 HTPASSED="/etc/nginx/.htpasswd"
 POST_EVENT_SCRIPT="/usr/local/bigbluebutton/core/scripts/post_events/update_analytics_data.rb"
 NGINX_CONFIG="/etc/bigbluebutton/analytics-dashboard.nginx"
+ROOT_PATH="$(npm root -g)/bigbluebutton-analytics"
 
 
 #set default user and password
 if [ ! -f $HTPASSED ]; then
     sudo touch $HTPASSED
-    bash ./bin/utils/create_or_update_user.sh admin admin123
+    bash $ROOT_PATH/bin/utils/create_or_update_user.sh admin admin123
 fi
 
 
 # Add post_event script
 if [  ! -f $POST_EVENT_SCRIPT ]; then
-    sudo cp -r ./update_analytics_data.rb  $POST_EVENT_SCRIPT
+    sudo cp -r $ROOT_PATH/update_analytics_data.rb  $POST_EVENT_SCRIPT
     echo "Adding post_event script"
 fi
 
@@ -36,9 +37,9 @@ fi
 #Deploy bbb-analytics
 if [  -f $BIGBLUEBUTTON ]; then 
     sudo mkdir -p /var/www/bigbluebutton-default/analytics
-    sudo  cp -r ./index.html ./data.json /var/www/bigbluebutton-default/analytics
+    sudo  cp -r $ROOT_PATH/index.html $ROOT_PATH/data.json /var/www/bigbluebutton-default/analytics
     sudo cp -r  /var/www/bigbluebutton-default/favicon.ico  /var/www/bigbluebutton-default/analytics/
-    node ./bin/utils/exportData.js
+    node $ROOT_PATH/bin/utils/exportData.js
     echo "Installation done."
     echo "You can access analytics-dashboard at https://<your-bbb>/analytics"
     exit 0
