6dcbabb7e73f9d9b6b55931462edef3dc6f11813
arun-hel <arun@higheredlab.com>
2022-03-21 09:47:20 +0000

    Added bg color

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import Tabels from "./components/table";
-import { Image, Typography, Layout, Menu, Space, Spin } from "antd";
+import { Image, Typography, Layout, Space, Spin } from "antd";
 import "./styles.css";
 import { useEffect, useState } from "react";
 import getData from './services/analytics.service'
-const { Header, Content, Footer } = Layout;
+const { Content, Footer } = Layout;
 const { Title, Text } = Typography;
 
 export default function App() {
@@ -17,24 +17,26 @@ export default function App() {
 
   }, [])
   return (
-
-    <div className="container-fluid px-5 ">
-      <div className="mt-4 mb-4">
-        <Title className="mb-1 pb-0 " level={3}>BigBlueButton Analytics</Title>
-        <Text className=" mt-0 pt-0 " >Access analytics of your BigBlueButton online classes</Text>
-      </div>
-      < div className="App" >
-        {data && data.length ?
-          <div className="rounded  mb-4 ">
-            <Tabels data={data} />
-          </div> :
-          <Space size="middle">
-            <Spin size="large" />
-          </Space>
-        }
-      </div>
-      <Footer className="bg-white" style={{ textAlign: 'center' }}><a href="https://higheredlab.com">HigherEdLab.com ©2022</a> </Footer>
-
-    </div >
+    <Layout className="layout">
+      <div className="container-fluid px-5 ">
+        <Content >
+          <div className="mt-4 mb-4">
+            <Title className="mb-1 pb-0 " level={3}>BigBlueButton Analytics</Title>
+            <Text className=" mt-0 pt-0 " >Access analytics of your BigBlueButton online classes</Text>
+          </div>
+          < div className="App" >
+            {data && data.length ?
+              <div className="rounded ">
+                <Tabels data={data} />
+              </div> :
+              <Space size="middle">
+                <Spin size="large" />
+              </Space>
+            }
+          </div>
+        </Content>
+        <Footer className="pt-3" style={{ textAlign: 'center' }}><a href="https://higheredlab.com">HigherEdLab.com ©2022</a> </Footer>
+      </div >
+    </Layout>
   );
 }
